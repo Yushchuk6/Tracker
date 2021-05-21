162344f807fa8e1e28eb39f3a73cfb6d3b40e3e9
yushchuk6 <yushchuk6@gmail.com>
2021-05-21 18:48:13 +0300

    new map and controls menu

--- view/plot.js
+++ view/plot.js
@@ -1,32 +1,75 @@
+const greenIcon = L.icon({
+    iconUrl: 'icons\\circle_green.png',
+
+    iconSize: [30, 30],
+    iconAnchor: [15, 15],
+    popupAnchor: [0, -20]
+});
+
+const purpleIcon = L.icon({
+    iconUrl: 'icons\\circle_purple.png',
+
+    iconSize: [30, 30],
+    iconAnchor: [15, 15],
+    popupAnchor: [0, -20]
+});
+
+const trackersStyle = {
+    color: 'red',
+    fillColor: 'rgba(0,0,0,0)',
+}
+
+const pathStyle = {
+    color: 'rgb(51, 136, 255)',
+}
+
+const hideStyle = {
+    color: 'rgba(0,0,0,0)',
+}
 
 class Plot {
-    constructor(id, data, layout, config) {
-        this.id = id;
-        this.data = data;
-        this.layout = layout;
-        this.config = config;
+    constructor(id) {
+        this.map = L.map(id);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFja3Jvc3MiLCJhIjoiY2tvdTdla2xzMDQwZjJub2IweXJhajc2MCJ9.BBNjH9bJ0U32S-r777oCLQ', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'your.mapbox.access.token'
+        }).addTo(this.map);
 
-        Plotly.newPlot(id, plot_data, plot_layout, plot_config);
+        this.path = L.polyline([], pathStyle).addTo(this.map);
+        this.trackers = L.polygon([], trackersStyle).addTo(this.map);
+        this.target = L.marker([0, 0], { icon: greenIcon }).addTo(this.map);
+        this.target_guess = L.marker([0, 0], { icon: purpleIcon }).addTo(this.map);
     }
 
-    update_trace(update) {
-        const trace = this.data.find(e => e.name === update.name);
-
-        this._add_to_dict(trace, update)
-
-        Plotly.redraw(this.id);
-        console.log(`[update] trace \'${update.name}\' updated`);
+    update_path(update) {
+        this.path.setLatLngs(update['latlon']);
+        this.map.fitBounds(this.path.getBounds());
     }
 
-    update_layout(update) {
-        Plotly.relayout(this.id, update)
-        console.log(`[update] layout updated`);
-        console.log(Plotly);
+    update_trackers(update) {
+        this.trackers.setLatLngs(update['latlon']);
     }
 
-    _add_to_dict(origin, update) {
-        for (const [key, value] of Object.entries(update)) {
-            origin[key] = value
+    update_target(update) {
+        this.target.setLatLng(update['latlon']);
+        this.target.bindPopup(`Target<br>${this.target.getLatLng()}`);
+    }
+
+    update_target_guess(update) {
+        this.target_guess.setLatLng(update['latlon']);
+        this.target_guess.bindPopup(`Guess Target<br>${this.target_guess.getLatLng()}`);
+    }
+
+    hide_path(state = true) {
+        if (state) {
+            this.path.setStyle(hideStyle);
+        }
+        else {
+            this.path.setStyle(pathStyle);
         }
     }
 }
--- view/websocket.js
+++ view/websocket.js
@@ -1,26 +1,36 @@
 const socket = new WebSocket('ws://localhost:8080');
 
 socket.onopen = function (e) {
-    console.log('[open] Opened connection');
+    console.log('[open] connection opened');
+};
+
+socket.onclose = function (e) {
+    console.log('[close] connection closed');
 };
 
 socket.onmessage = function (event) {
     const command = JSON.parse(event.data);
     const data = command.data
-    console.log(data)
-    switch (command.type){
+
+    switch (command.type) {
         case 'message':
             console.log(`[message]: ${data}`);
             break;
-        case 'trace':
-            plot.update_trace(data);
+        case 'path':
+            map.update_path(data);
+            console.log(`[update] path updated`);
             break;
-        case 'layout':
-            plot.update_layout(data);
+        case 'trakers':
+            map.update_trackers(data);
+            console.log(`[update] trakers updated`);
             break;
-        case 'slider':
-            const slider = document.getElementById('slider');
-            set_list_attributes(slider, data)
+        case 'target':
+            map.update_target(data);
+            console.log(`[update] target updated`);
+            break;
+        case 'target_guess':
+            map.update_target_guess(data);
+            console.log(`[update] target guess updated`);
             break;
         case 'test':
             console.log(`[test] test data: ${data}`);
@@ -28,11 +38,11 @@ socket.onmessage = function (event) {
     }
 };
 
-function send_socket(message){
+function send_socket(message) {
     socket.send(message)
 }
 
-function set_list_attributes(html_el, attr){
+function set_list_attributes(html_el, attr) {
     for (const [key, value] of Object.entries(attr)) {
         html_el.setAttribute(key, value);
     }
